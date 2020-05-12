85c062129523b932d36a8f82d710d6c77c5de8fa
Calvin Davidson <calvin2003davidson@gmail.com>
2020-05-12 09:54:58 +0200

    Added answer shuffling.

--- script.js
+++ script.js
@@ -16,98 +16,106 @@ let hasAnswered = false;
 
 let VragenVolgorde = [];
 
-function init(){
-  Questions = getAllQuestions();
-  // haal de data op met AJAX
-  makeAjaxCall (quizJsonFile, "GET").then(handleReceivedData); // doe het! wacht op promise
-  function handleReceivedData(jsonString){ // pak de data aan
-    quiz = JSON.parse(jsonString);
-    //console.log(quiz); // debug
-    initQuiz(); // start de quiz
-  }
-}
-
-function initQuiz(){
-  // reset alle player game variabelen
-  playerData.goodAnswers = 0;
-  playerData.wrongAnswers = 0;
-  playerData.name = prompt("wat is uw naam?"); // toekomstige uitbreiding naam speler opvragen
-  
-  VragenVolgorde = [];
-
-  for (let i = 0; i < quiz.quizContent.length; i++) {
-    VragenVolgorde.push(i);
-  }
-  VragenVolgorde = shuffleArray(VragenVolgorde);
-
-  resultBox.style.display = "none"; // verberg de resultbox
-  prepareQuestions(); // start de quiz
-}
-
-function prepareQuestions() {
-  hasAnswered = false;
-  questionBox.className = "questionBox-new";
-
-  quizWrapper.style.backgroundImage = "url("+ quiz.quizMetaData.imageURI; + ")";
-
-  if (counter < VragenVolgorde.length) {
-    myQuestion.innerHTML = quiz.quizContent[VragenVolgorde[counter]].question;
-    myAnswer.innerHTML = "";
-    for (let i = 0; i < quiz.quizContent[VragenVolgorde[counter]].answers.length; i++) {
-
-      let answer = document.createElement('li');
-      answer.className = "answer";
-      answer.score = quiz.quizContent[VragenVolgorde[counter]].answers[i].feedback;
-      answer.innerHTML = quiz.quizContent[VragenVolgorde[counter]].answers[i].answer;
-
-      myAnswer.appendChild(answer);
-      myAnswer.addEventListener('click', evaluate, true)
+function init() {
+    Questions = getAllQuestions();
+    // haal de data op met AJAX
+    makeAjaxCall(quizJsonFile, "GET").then(handleReceivedData); // doe het! wacht op promise
+    function handleReceivedData(jsonString) { // pak de data aan
+        quiz = JSON.parse(jsonString);
+        //console.log(quiz); // debug
+        initQuiz(); // start de quiz
     }
-  } else {
-    finishQuiz();
-  }
 }
 
+// Reset de score van hiervoor, en shuffeld de vragenlijst.
+function initQuiz() {
+    // reset alle player game variabelen
+    playerData.goodAnswers = 0;
+    playerData.wrongAnswers = 0;
+    playerData.name = prompt("wat is uw naam?"); // toekomstige uitbreiding naam speler opvragen
+
+    VragenVolgorde = [];
+
+    for (let i = 0; i < quiz.quizContent.length; i++) {
+        VragenVolgorde.push(i);
+    }
+    VragenVolgorde = shuffleArray(VragenVolgorde);
+
+    resultBox.style.display = "none"; // verberg de resultbox
+    prepareQuestions(); // start de quiz
+}
+
+// laat de vraag en de lijst zien.
+function prepareQuestions() {
+    if (counter < VragenVolgorde.length) {
+
+        hasAnswered = false;
+        questionBox.className = "questionBox-new";
+
+        quizWrapper.style.backgroundImage = "url(" + quiz.quizMetaData.imageURI + ")";
+        myQuestion.innerHTML = quiz.quizContent[VragenVolgorde[counter]].question;
+        myAnswer.innerHTML = "";
+
+        let answers = [];
+        for (let i = 0; i < quiz.quizContent[VragenVolgorde[counter]].answers.length; i++) {
+
+            let answer = document.createElement('li');
+            answer.className = "answer";
+            answer.score = quiz.quizContent[VragenVolgorde[counter]].answers[i].feedback;
+            answer.innerHTML = quiz.quizContent[VragenVolgorde[counter]].answers[i].answer;
+            answer.addEventListener('click', evaluate, true)
+            answers.push(answer);
+        }
+
+        answers = shuffleArray(answers);
+        for (let i = 0; i < answers.length; i++) {
+            myAnswer.appendChild(answers[i]);
+        }
+
+    } else {
+        finishQuiz();
+    }
+}
+
+// Kijkt of het gegeven antwoord goed of fout is.
 function evaluate(evt) {
-  if (hasAnswered) return;
-  if (evt.target.className === "myAnswer") return;
+    if (hasAnswered) return;
 
-  if (evt.target.score) {
-    evt.target.className = "right";
-    playerData.goodAnswers += 1; // increase good score
-  } else {
-    evt.target.className = "wrong";
-    playerData.wrongAnswers += 1; // increase wrong score
-  }
-  counter++;
-  questionBox.className = "questionBox";
-  
-  hasAnswered = true;
-  setTimeout(prepareQuestions, delayTime);
+    if (evt.target.score) {
+        evt.target.className = "right";
+        playerData.goodAnswers += 1; // increase good score
+    } else {
+        evt.target.className = "wrong";
+        playerData.wrongAnswers += 1; // increase wrong score
+    }
+    counter++;
+    questionBox.className = "questionBox";
+
+    hasAnswered = true;
+    setTimeout(prepareQuestions, delayTime);
 }
+
 
 // word getriggerd wanneer de quiz af is.
 function finishQuiz() {
-  hasAnswered = true;
+    myAnswer.innerHTML = "";
+    myQuestion.innerHTML = "results";
 
-  myAnswer.innerHTML = "";
-  myQuestion.innerHTML = "results";
+    let Name = document.createElement('li');
+    Name.className = "Result";
+    Name.innerHTML = "Name: " + playerData.name;
 
-  let answer1 = document.createElement('li');
-  answer1.className = "Result";
-  answer1.innerHTML = "Name: " + playerData.name;
+    let GoodPoints = document.createElement('li');
+    GoodPoints.className = "Result";
+    GoodPoints.innerHTML = "Goede antwoorden: " + playerData.goodAnswers;
 
-  let answer2 = document.createElement('li');
-  answer2.className = "Result";
-  answer2.innerHTML = "Goede antwoorden: " + playerData.goodAnswers;
+    let WrongPoints = document.createElement('li');
+    WrongPoints.className = "Result";
+    WrongPoints.innerHTML = "foute antwoorden: " + playerData.wrongAnswers;
 
-  let answer3 = document.createElement('li');
-  answer3.className = "Result";
-  answer3.innerHTML = "foute antwoorden: " + playerData.wrongAnswers;
-
-  myAnswer.appendChild(answer1);
-  myAnswer.appendChild(answer2);
-  myAnswer.appendChild(answer3);
+    myAnswer.appendChild(Name);
+    myAnswer.appendChild(GoodPoints);
+    myAnswer.appendChild(WrongPoints);
 }
 
 init();
